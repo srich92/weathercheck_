0ddc67eaf32799f4f6b782a9f00997d75c001316
srich92 <srichards646@gmail.com>
2022-11-07 18:48:44 -0500

    menu button is working

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,12 @@
 
 var apiKey = "41a254e0e763bf58b5ab03ce00cd9598";
+let nav_display = false;
+
+document.getElementById("menu").onclick = function () {
+    nav_display = !nav_display;
+    if (nav_display) {
+        document.getElementById("side-nav").style.display = 'block';
+    } else {
+        document.getElementById("side-nav").style.display = 'none';
+    }
+}
